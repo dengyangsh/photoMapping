243c14d6e20d791ebb509bb6a621ac0ab44402e7
dengbin <dengbin@hebao.im>
2018-11-29 15:03:26 +0800

    mod 增加参数校验和打包配置

--- src/main/java/com/photoMapping/controller/PhotoController.java
+++ src/main/java/com/photoMapping/controller/PhotoController.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.photoMapping.constant.ErrorCode;
 import com.photoMapping.service.PhotoServiceApi;
 import com.photoMapping.util.Response;
 
@@ -31,9 +33,14 @@ public class PhotoController {
 	@RequestMapping(value = "uploadPhoto", method = RequestMethod.POST)
 	public Response uploadPhoto(@RequestParam("photo") MultipartFile photo, String province, Integer userId)
 			throws Exception {
+		// 参数判断
+		if (StringUtils.isEmpty(province) || userId == null) {
+			return Response.error(ErrorCode.ERROR, "参数为空请确认");
+
+		}
 		// 接受文件
 		String rootPath = "/usr/java/photo";
-		File dir = new File(rootPath + File.separator + "tmpFiles");
+		File dir = new File(rootPath + File.separator + userId + province);
 		if (!dir.exists())
 			dir.mkdirs();
 		// 写文件到服务器
