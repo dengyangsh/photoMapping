97e5a9b3a2496c683295bbfc353c9fce6820bb83
dengbin <dengbin@hebao.im>
2018-11-30 09:30:16 +0800

    mod mybatis注解自動搭建

--- src/main/java/com/photoMapping/dao/PhotoDao.java
+++ src/main/java/com/photoMapping/dao/PhotoDao.java
@@ -2,10 +2,17 @@ package com.photoMapping.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.type.JdbcType;
+
+import com.photoMapping.model.Photo;
 
 @Mapper
 public interface PhotoDao {
@@ -17,4 +24,63 @@ public interface PhotoDao {
 	void save(@Param(value = "url") String url, @Param(value = "userId") Integer userId,
 			@Param(value = "province") String province);
 
+	
+	
+	  @Delete({
+	        "delete from photo",
+	        "where id = #{id,jdbcType=INTEGER}"
+	    })
+	    int deleteByPrimaryKey(Integer id);
+
+	    @Insert({
+	        "insert into photo (id, userId, ",
+	        "province, city, ",
+	        "createTime, url)",
+	        "values (#{id,jdbcType=INTEGER}, #{userid,jdbcType=INTEGER}, ",
+	        "#{province,jdbcType=VARCHAR}, #{city,jdbcType=VARCHAR}, ",
+	        "#{createtime,jdbcType=TIMESTAMP}, #{url,jdbcType=VARCHAR})"
+	    })
+	    int insert(Photo record);
+
+	    @Select({
+	        "select",
+	        "id, userId, province, city, createTime, url",
+	        "from photo",
+	        "where id = #{id,jdbcType=INTEGER}"
+	    })
+	    @Results({
+	        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+	        @Result(column="userId", property="userid", jdbcType=JdbcType.INTEGER),
+	        @Result(column="province", property="province", jdbcType=JdbcType.VARCHAR),
+	        @Result(column="city", property="city", jdbcType=JdbcType.VARCHAR),
+	        @Result(column="createTime", property="createtime", jdbcType=JdbcType.TIMESTAMP),
+	        @Result(column="url", property="url", jdbcType=JdbcType.VARCHAR)
+	    })
+	    Photo selectByPrimaryKey(Integer id);
+
+	    @Select({
+	        "select",
+	        "id, userId, province, city, createTime, url",
+	        "from photo"
+	    })
+	    @Results({
+	        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+	        @Result(column="userId", property="userid", jdbcType=JdbcType.INTEGER),
+	        @Result(column="province", property="province", jdbcType=JdbcType.VARCHAR),
+	        @Result(column="city", property="city", jdbcType=JdbcType.VARCHAR),
+	        @Result(column="createTime", property="createtime", jdbcType=JdbcType.TIMESTAMP),
+	        @Result(column="url", property="url", jdbcType=JdbcType.VARCHAR)
+	    })
+	    List<Photo> selectAll();
+
+	    @Update({
+	        "update photo",
+	        "set userId = #{userid,jdbcType=INTEGER},",
+	          "province = #{province,jdbcType=VARCHAR},",
+	          "city = #{city,jdbcType=VARCHAR},",
+	          "createTime = #{createtime,jdbcType=TIMESTAMP},",
+	          "url = #{url,jdbcType=VARCHAR}",
+	        "where id = #{id,jdbcType=INTEGER}"
+	    })
+	    int updateByPrimaryKey(Photo record);
 }
--- src/main/java/com/photoMapping/dao/UserDao.java
+++ src/main/java/com/photoMapping/dao/UserDao.java
@@ -1,7 +1,15 @@
 package com.photoMapping.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.type.JdbcType;
 
 import com.photoMapping.model.User;
 
@@ -11,4 +19,56 @@ public interface UserDao {
 	@Select("SELECT * FROM user WHERE id = #{id}")
 	User findUserById(Integer id);
 
+	
+	
+	
+    @Delete({
+        "delete from user",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    int deleteByPrimaryKey(Integer id);
+
+    @Insert({
+        "insert into user (id, phone, ",
+        "createTime, realName)",
+        "values (#{id,jdbcType=INTEGER}, #{phone,jdbcType=VARCHAR}, ",
+        "#{createtime,jdbcType=TIMESTAMP}, #{realname,jdbcType=VARCHAR})"
+    })
+    int insert(User record);
+
+    @Select({
+        "select",
+        "id, phone, createTime, realName",
+        "from user",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+        @Result(column="phone", property="phone", jdbcType=JdbcType.VARCHAR),
+        @Result(column="createTime", property="createtime", jdbcType=JdbcType.TIMESTAMP),
+        @Result(column="realName", property="realname", jdbcType=JdbcType.VARCHAR)
+    })
+    User selectByPrimaryKey(Integer id);
+
+    @Select({
+        "select",
+        "id, phone, createTime, realName",
+        "from user"
+    })
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER, id=true),
+        @Result(column="phone", property="phone", jdbcType=JdbcType.VARCHAR),
+        @Result(column="createTime", property="createtime", jdbcType=JdbcType.TIMESTAMP),
+        @Result(column="realName", property="realname", jdbcType=JdbcType.VARCHAR)
+    })
+    List<User> selectAll();
+
+    @Update({
+        "update user",
+        "set phone = #{phone,jdbcType=VARCHAR},",
+          "createTime = #{createtime,jdbcType=TIMESTAMP},",
+          "realName = #{realname,jdbcType=VARCHAR}",
+        "where id = #{id,jdbcType=INTEGER}"
+    })
+    int updateByPrimaryKey(User record);
 }
